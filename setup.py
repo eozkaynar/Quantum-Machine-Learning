931397afb4262905f16f02d641a309aec545a7d6
eozkaynar <eozkaynar575@gmail.com>
2025-05-18 12:37:59 +0300

    requirements are changed

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ setup(
     name='quantum_mlp_optimization',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        "torch",
-        "torchvision",
-        "numpy",
-        "scikit-learn",
-        "matplotlib",
-        "pennylane",
-        "pennylane-qiskit",
-        "qiskit",
-        "pandas",
-        "seaborn",
-        "Click",
-        "tqdm"
-    ],
+    # install_requires=[
+    #     "torch",
+    #     "torchvision",
+    #     "numpy",
+    #     "scikit-learn",
+    #     "matplotlib",
+    #     "pennylane",
+    #     "pennylane-qiskit",
+    #     "qiskit",
+    #     "pandas",
+    #     "seaborn",
+    #     "Click",
+    #     "tqdm"
+    # ],
 )
